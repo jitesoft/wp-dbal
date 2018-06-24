d84da648632de270340d57d3543e27ce5adb0b2f
Johannes <johannes@jitesoft.com>
2018-06-24 19:44:09 +0200

    Removed sast/dependency jobs.

---
+++ src/Models/Metadata/RelationMetadata.php
@@ -0,0 +1,16 @@
+<?php
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  RelationMetadata.php - Part of the wordpress-database-abstraction project.
+
+  © - Jitesoft 2018
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+namespace Jitesoft\WordPress\DBAL\Models\Metadata;
+
+/**
+ * RelationMetadata
+ * @author Johannes Tegnér <johannes@jitesoft.com>
+ * @version 1.0.0
+ */
+class RelationMetadata {
+
+}
